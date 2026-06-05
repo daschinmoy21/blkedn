b48380a278265c19a22c7b50112f713de45f3155
daschinmoy21 <daschinmoyy21@gmail.com>
2026-06-05 20:05:06 +0530

    fix issues

--- host/greeter.nix
+++ host/greeter.nix
@@ -1,19 +1,21 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  tuigreetCommand = pkgs.writeShellScript "tuigreet-session" ''
+    exec ${pkgs.tuigreet}/bin/tuigreet \
+      --time \
+      --asterisks \
+      --user-menu \
+      --remember \
+      --greeting WELCOME \
+      --theme 'text=red;prompt=green;time=red;input=red;border=white;title=red;action=white;greet=white' \
+      --cmd ${pkgs.niri}/bin/niri-session
+  '';
+in {
   services.greetd = {
     enable = true;
     useTextGreeter = true;
     settings = {
       default_session = {
-        command = ''
-          ${pkgs.tuigreet}/bin/tuigreet \
-            --time \
-            --asterisks \
-            --user-menu \
-            --remember \
-            --greeting WELCOME \
-            --theme text=red;prompt=green;time=red;input=red;border=white;title=red;action=white;greet=white \
-            --cmd niri-session
-        '';
+        command = "${tuigreetCommand}";
         user = "greeter";
       };
     };
